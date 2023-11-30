f8de7b6f49d3880d2af956d712c1e727a082e630
nicolasgsantana <nicolas07san@gmail.com>
2023-11-29 21:44:06 -0300

    Add form functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const dialog = document.querySelector("dialog");
 const addBookBtn = document.querySelector(".section-header button");
 const closeDialogBtn = document.querySelector("dialog>button");
+const submitBtn = document.getElementById("submit");
+
+const form = document.querySelector("form");
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookReadStatus = document.getElementById("has-read");
 
 const myLibrary = [];
 
@@ -9,10 +16,11 @@ function Book(title, author, pages, hasRead) {
     this.author = author;
     this.pages = pages;
     this.hasRead = hasRead;
+    this.id = myLibrary.length;
 }
 
 function addBookToLibrary(title, author, pages, hasRead) {
-    myLibrary.push(new Book(title, author, pages, hasRead));
+    myLibrary.push(new Book(title, author, Number(pages), hasRead === "on" ? true : false));
 }
 
 function showBooks() {
@@ -26,5 +34,12 @@ addBookBtn.addEventListener("click", () => {
 
 closeDialogBtn.addEventListener("click", () => {
     dialog.close();
+});
+
+submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    dialog.close();
+    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookReadStatus.value);
+    form.reset();
 });
 
